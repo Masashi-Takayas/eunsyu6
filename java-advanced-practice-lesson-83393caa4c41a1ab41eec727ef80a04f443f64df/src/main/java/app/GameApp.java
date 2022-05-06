6a63f47ee8e7a5d2208a3b8383771bb8168d9dd7
Masashi-Takayas <takayasu_masashi@axiz.co.jp>
2022-05-06 18:00:16 +0900

    test

---
+++ java-advanced-practice-lesson-83393caa4c41a1ab41eec727ef80a04f443f64df/src/main/java/app/GameApp.java
@@ -0,0 +1,22 @@
+package app;
+
+public class GameApp {
+
+	public String item;
+	
+	public GameApp() {
+		
+	}
+	
+	public GameApp(String name) {
+		name = item;
+	}
+	
+	public String start(String name) {
+		String fight;
+		
+		fight = name+"さんと"+item+"でゲームを開始します";
+		
+		return fight;
+	}
+}
